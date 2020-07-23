6d9df93f1855d8012466306c6075b4d948250ff7
diwashrestha <diwash712@gmail.com>
2020-07-23 23:26:21 +0545

    Added Simplest estimation stage 1

---
+++ Readibility-Score/Readibility-Score/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Readibility_Score
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Sentences: ");
+            string userInput = Console.ReadLine();
+            Userinput sentence = new Userinput(userInput);
+            Console.WriteLine(sentence.ReadScore());
+        }
+    }
+}
---
+++ Readibility-Score/Readibility-Score/Userinput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Readibility_Score
+{
+    public class Userinput
+    {
+        public string UserInputs { get; }
+
+        // Constructor Declaration
+        public Userinput(String userInput )
+        {
+            UserInputs = userInput;
+        }
+
+        public String ReadScore()
+        {
+            string message;
+            if(UserInputs.Length > 100)
+            {
+                message = "Hard";
+            }
+            else
+            {
+                message = "easy";
+            }
+            return (message);
+
+        }
+    }
+}
