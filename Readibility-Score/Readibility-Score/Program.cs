c4f154cede8f5eb6e25e6b9c4dbc3839243a1542
diwashrestha <diwash712@gmail.com>
2020-07-29 20:39:24 +0545

    Added Syllables and Polysyllables

--- Readibility-Score/Readibility-Score/Program.cs
+++ Readibility-Score/Readibility-Score/Program.cs
@@ -6,10 +6,18 @@ namespace Readibility_Score
     {
         static void Main(string[] args)
         {
+            Start:
             Console.WriteLine("Enter Your Sentences: ");
             string userInput = Console.ReadLine();
             Userinput sentence = new Userinput(userInput);
-            Console.WriteLine(sentence.ReadScore());
+            sentence.ReadScore();
+            sentence.AriTest();
+            sentence.syllablesCount();
+            Console.WriteLine("Check more Sentences: Yes[Y]/No[N]?");
+            if(Console.ReadLine() == "Y")
+            {
+                goto Start;
+            }
         }
     }
 }
--- Readibility-Score/Readibility-Score/Userinput.cs
+++ Readibility-Score/Readibility-Score/Userinput.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Readibility_Score
 {
     public class Userinput
     {
         public string UserInputs { get; }
+        private int characterCount = 0;
+        private int wordCount = 0;
+        private int sentenceCount = 0;
+        private int syllableCount = 0;
+        private int polysyllableCount = 0;
 
         // Constructor Declaration
         public Userinput(String userInput )
@@ -15,29 +21,185 @@ namespace Readibility_Score
             UserInputs = userInput;
         }
 
-        public String ReadScore()
+
+        // Method for counting the word, character, sentence, syllables, polysyllables
+        public void ReadScore()
         {
-            string message;
-            int wordcount = 0;
-            int sentencecount = 0;
             char[] charsToTrim = { '.', '!' };
 
-            // Counting Words in Each Sentences
+            // Separate each sentence in the given string
             foreach (string userinput in UserInputs.TrimEnd(charsToTrim).Split(charsToTrim))
             {
-                wordcount += userinput.Trim().Split(' ').Count();
-                sentencecount++;
-                Console.WriteLine("WordCount:{0}\n Sentence Count:{1}", wordcount, sentencecount);
+                wordCount += userinput.Trim().Split(' ').Count();
+                sentenceCount++;
             }
-
-            if (wordcount / sentencecount > 10)
+            foreach(string word in UserInputs.Split(' '))
             {
-                message = "The Text is Hard.";
+                characterCount += word.Length;
             }
-            else
-                message = "The Text is Easy.";
 
-            return message;
+            Console.WriteLine( $"Words: {wordCount}\n Sentences: {sentenceCount} \n Characters: {characterCount} \n ");
+        }
+
+        public void syllablesCount()
+        {
+            char[] charsToTrim = { '.', '!', '?' };
+            char[] vowelLetter = { 'a', 'e', 'i', 'o', 'u', 'y' };
+            bool prevVowel;
+            bool isPolysyllable = false;
+            int localSyllableCount = 0;
+            foreach (string userinput in UserInputs.TrimEnd(charsToTrim).Split(charsToTrim))
+            {
+                foreach (string word in userinput.Split(' '))
+                {
+                    prevVowel = false;
+                    isPolysyllable = false;
+                    localSyllableCount = 0;
+                    int i = 0;
+                    int wordLength = word.Length;
+                    foreach (char character in word.ToLower())
+                    {
+                        bool isLastLetter = i == (wordLength - 1);
+                        //Rule3: if last letter in word is 'e' do not count as a vowel.
+                        if (isLastLetter == true && character.Equals('e'))
+                        {
+
+                        }
+                        else if (Array.Exists(vowelLetter, element => element == character))
+                        {
+
+                            // Rule 1: Count the Number of vowel
+                            if (prevVowel == false)
+                            {
+                                localSyllableCount++;
+                                prevVowel = true;
+                            }
+                            // Rule 2: Do Not count double vowels
+                            else if(prevVowel == true)
+                            {
+                                prevVowel = true;
+                            }
+                        }
+                        else
+                        {
+                            prevVowel = false;
+                        }
+
+                        if(localSyllableCount>2 && isPolysyllable == false)
+                        {
+                            polysyllableCount++;
+                            isPolysyllable = true;
+                        }
+
+                        Console.WriteLine("syllable:{0}\n polysyllable:{1}", localSyllableCount,polysyllableCount);
+                        i++;
+                    }
+
+                    // Rule 4:
+                    // if a word contains 0 vowel then syllable is 1
+                    if(localSyllableCount == 0)
+                    {
+                        localSyllableCount = 1;
+                    }
+                    syllableCount += localSyllableCount;
+                }
+            }
+            Console.WriteLine($"Syllables: {syllableCount}");
+
+        }
+
+
+        public void RegexSyllablesCount()
+        {
+            char[] charsToTrim = { '.', '!', '?' };
+            char[] vowelLetter = { 'a', 'e', 'i', 'o', 'u', 'y' };
+            bool prevVowel;
+            bool isPolysyllable = false;
+            int localSyllableCount = 0;
+            foreach (string userinput in UserInputs.TrimEnd(charsToTrim).Split(charsToTrim))
+            {
+                string syllabPattern = @"(?i)[aiou][aeiou]*|e[aeiou]*(?!d?\\b)";
+                Regex regexPattern = new Regex(syllabPattern);
+
+                foreach (string word in userinput.Split(' '))
+                {
+                    if (regexPattern.IsMatch(word))
+                    {
+                        localSyllableCount++;
+                    }
+                    Console.WriteLine("syllable:{0}\n polysyllable:{1}", localSyllableCount, polysyllableCount);
+                }
+                if(localSyllableCount>2)
+                {
+                    polysyllableCount++;
+                }
+            } 
+        }
+
+
+        // Method for Automated Readability Index
+        public void AriTest()
+        {
+            double score = 0;
+            string ageGroup = "";
+            score = ((4.71 * (characterCount / wordCount)) + (0.5 * (wordCount / sentenceCount) - 21.43));
+
+            if (score < 2)
+            {
+                ageGroup = "5-6";
+            }
+            else if (score < 3)
+            {
+                ageGroup = "6-7";
+            }
+            else if (score < 4)
+            {
+                ageGroup = "7-9";
+            }
+            else if (score < 5)
+            {
+                ageGroup = "9-10";
+            }
+            else if (score < 6)
+            {
+                ageGroup = "10-11";
+            }
+            else if (score < 7)
+            {
+                ageGroup = "11-12";
+            }
+            else if (score < 8)
+            {
+                ageGroup = "12-13";
+            }
+            else if (score < 9)
+            {
+                ageGroup = "13-14";
+            }
+            else if (score < 10)
+            {
+                ageGroup = "15-16";
+            }
+            else if (score < 11)
+            {
+                ageGroup = "17-18";
+            }
+            else if (score < 12)
+            {
+                ageGroup = "18-24";
+            }
+            else if (score < 13)
+            {
+                ageGroup = "24+";
+            }
+
+            Console.WriteLine($"The score is: {score:N2} \n This text should be understood by {ageGroup} year olds.");
+        }
+
+        // Flesch–Kincaid readability tests
+        public void FkrTest()
+        {
+
         }
     }
 }
